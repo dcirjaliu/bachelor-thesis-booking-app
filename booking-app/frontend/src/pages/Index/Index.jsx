48934e1786fb7f7fbd1fb0dda55217bed100843c
dcirjaliu <diana.cirjaliu@student.upt.ro>
2026-02-13 20:38:21 +0200

    feat(component):implement header

--- booking-app/frontend/src/pages/Index/Index.jsx
+++ booking-app/frontend/src/pages/Index/Index.jsx
@@ -1,15 +1,13 @@
 import styles from "./Index.module.scss";
 import AccentShape from "../../components/AccentShape/AccentShape";
 import HeroImage from "../../components/HeroImage/HeroImage";
+import Header from "../../components/Header/Header";
 
 function Index() {
   return (
     <div className={styles.snapContainer}>
       <section className={styles.page} id="page1">
-        <div className={styles.page1}>
-          <AccentShape />
-          <HeroImage />
-        </div>
+        <Header />
       </section>
       <section className={styles.page} id="page2">
         Page 2
