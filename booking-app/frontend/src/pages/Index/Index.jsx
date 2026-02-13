9935f7bd701088508d1217707250f43611f9dee6
dcirjaliu <diana.cirjaliu@student.upt.ro>
2026-02-13 22:06:11 +0200

    feat(index page):started implementing the strcuture for the main page

--- booking-app/frontend/src/pages/Index/Index.jsx
+++ booking-app/frontend/src/pages/Index/Index.jsx
@@ -2,12 +2,23 @@ import styles from "./Index.module.scss";
 import AccentShape from "../../components/AccentShape/AccentShape";
 import HeroImage from "../../components/HeroImage/HeroImage";
 import Header from "../../components/Header/Header";
+import "../../global_styles/_typography.scss";
 
 function Index() {
   return (
     <div className={styles.snapContainer}>
-      <section className={styles.page} id="page1">
+      <section className={`${styles.page} ${styles.page1}`} id="page1">
         <Header />
+        <div className={styles.contentContainer}>
+          <div className={styles.gridItem}>
+            <div className="typewriter">
+              <h1>Rezervă-ți timpul, nu doar locul.</h1>
+            </div>
+          </div>
+          <div className={styles.gridItem}>
+            <HeroImage />
+          </div>
+        </div>
       </section>
       <section className={styles.page} id="page2">
         Page 2
