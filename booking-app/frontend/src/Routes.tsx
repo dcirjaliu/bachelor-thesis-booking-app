65ce12dcc4c25152ca41258e5eac1244e00530fd
dcirjaliu <diana.cirjaliu@student.upt.ro>
2026-02-19 10:06:06 +0200

    feat(ui):implemented the structure of service-details page

--- booking-app/frontend/src/Routes.tsx
+++ booking-app/frontend/src/Routes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import IndexPage from "./pages/Index/Index";
+import ServiceDetails from "./pages/ServiceDetails/ServiceDetails";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<IndexPage />} />
+        <Route path="/servicedetails" element={<ServiceDetails />} />
       </Routes>
     </BrowserRouter>
   );
