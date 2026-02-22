806ee972e4101a5e2b3a0b43f18836ed51400fa5
dcirjaliu <diana.cirjaliu@student.upt.ro>
2026-02-22 18:18:07 +0200

    feat(ui):finished the ui for the service details page

--- booking-app/frontend/src/pages/ServiceDetails/ServiceDetails.jsx
+++ booking-app/frontend/src/pages/ServiceDetails/ServiceDetails.jsx
@@ -1,12 +1,59 @@
 import Grid from "@mui/material/Grid";
 import Header from "../../components/Header/Header";
+import { serviceDetails } from "../../assets/data/serviceDetails";
+import styles from "./ServiceDetails.module.scss";
+import Pagination from "@mui/material/Pagination";
+import { useMemo } from "react";
+import { useHeader } from "../../hooks/useHeader";
+import Button from "../../components/Button/Button";
 
 function ServiceDetails() {
+  const headerButtons = useMemo(
+    () => (
+      <>
+        <Button variant="primary">Intră în cont</Button>
+        <Button variant="secondary">Contact</Button>
+      </>
+    ),
+    [],
+  );
+
+  useHeader(headerButtons, styles.btnsContainerHeader);
   return (
-    <Grid container spacing={4} alignItems="center">
-      <Header />
-      <Grid item xs={12} md={5}></Grid>
-      <Grid item xs={12} md={7}></Grid>
+    <Grid container spacing={4} alignItems="stretch">
+      <Grid
+        item
+        xs={12}
+        md={5}
+        sx={{ padding: 3, display: { xs: "none", md: "block" } }}
+      >
+        <img src={serviceDetails[0].image} />
+      </Grid>
+      <Grid item xs={12} md={7} sx={{ padding: 3 }}>
+        {serviceDetails.map((s) => (
+          <div key={s.id} className={styles.infoCard}>
+            <h2>{s.title}</h2>
+            <h3>{s.location}</h3>
+            <p>{s.description}</p>
+            <Pagination
+              count={3}
+              size="small"
+              sx={{
+                marginTop: "auto",
+                "& .MuiPaginationItem-root": {
+                  color: "white",
+                },
+                "& .Mui-selected": {
+                  backgroundColor: "rgba(255, 255, 255, 0.3) !important",
+                  color: "white",
+                },
+                alignSelf: "center",
+                paddingBottom: 2,
+              }}
+            />
+          </div>
+        ))}
+      </Grid>
     </Grid>
   );
 }
